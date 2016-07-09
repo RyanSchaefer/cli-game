09a1c52e0e2d60366d1282bc45ed703bda879c6d
Dev Squid <contact.devsquid@gmail.com>
2016-07-09 17:02:49 -0500

    Working version of a class system
    overhauled old version

--- driver.js
+++ driver.js
@@ -1,4 +1,139 @@
-var animationDone=!0;function g_e(a){return document.getElementById(a)}var Main=function(){this.animations=[];this.tasks=[]};Main.prototype.start_animating=function(){setInterval(function(){if(!0===animationDone)try{this.animations[0](),this.animations.shift()}catch(a){}},1)};Main.prototype.start_working=function(){setInterval(function(){try{this.tasks[0](),this.tasks.shift()}catch(a){}},1)};
-Main.prototype.animation_blink=function(){var a=0;window.setInterval(function(){0===a?(document.getElementById("blinker").innerHTML=">",a=1):1==a&&(document.getElementById("blinker").innerHTML="&nbsp;",a=0)},1E3)};Main.prototype.animation_fade=function(a){var b=0,c=0;animationDone=!1;var e=setInterval(function(){document.getElementById(a).style.opacity=b;1<b&&0===c?c=1:0>b?clearInterval(e):0===c?b+=.2:1==c&&(b-=.2)},500);animationDone=!0};
-Main.prototype.animation_plbl=function(a,b,c){function e(){clearInterval(d[a]);delete d[a]}animationDone=!1;var d=this.animation_plbl.intervals;d||(this.animation_plbl.intervals=d={});d[a]&&e();var f=0,g=document.getElementById(a);d[a]=setInterval(function(){"|"==b.charAt(f)?g.innerHTML+="<br>":g.innerHTML+=b.charAt(f);f++;f>b.length&&(animationDone=!0,e())},c)};Main.prototype.command=function(a){};Main.prototype.get_input=function(){var a=g_e("input").value;g_e("input").value="";return a};
-Main.prototype.key_checker=function(a){return 13==a.keyCode?(console.log(this.get_input()),!1):!0};Main.prototype.start=function(){this.start_working();this.start_animating();this.animation_blink()};var main=new Main;main.start();
+//window.onerror = function (error){
+//alert(error);
+//};
+//Start up animation
+function g_e(e){
+	return document.getElementById(e);
+};
+animation_done = true;
+class Main{
+	constructor(){
+		this.animations = [];
+		this.tasks = [];
+		this.context = "";
+		this.path = "";
+	}
+	start_animating(){
+		try{
+			if (animation_done){
+				var y = this.animations.shift();
+				y.func(y.args);
+			}
+		}
+		catch(e){}
+	}
+	add_animation(f, a){
+		this.animations.push({func:f, args:a});
+	}
+	start_working(){
+		try{
+			var y = this.tasks.shift();
+			y.func(y.args);
+		}
+		catch(e){}
+	}
+	add_task(f, a){
+		this.tasks.push({func:f, args:a});
+	}
+	animation_blink(){
+		var blink = 0;
+		window.setInterval(function blinking(){
+			if (blink == 0){
+				document.getElementById("blinker").innerHTML = ">";
+				blink= 1;
+			}
+			else if(blink == 1){
+				document.getElementById("blinker").innerHTML="&nbsp;";
+				blink = 0;
+			}
+		}, 1000);
+	}
+	animation_fade(object){
+		animation_done = false;
+		var fade = 0;
+		var fadeOut = 0;
+		var interval = setInterval(function startFade(){
+			g_e(object.element).style.opacity = fade;
+			if(fade>1 && fadeOut == 0){
+				fadeOut = 1;
+			}
+			else if(fade < 0){
+				animation_done = true;
+				fadeOut = 0;
+				clearInterval(interval);
+			}
+			else if(fadeOut == 0){
+				fade = fade + 0.2;
+			}
+			else if(fadeOut == 1){
+				fade = fade - 0.2;
+			}
+		}, 500);
+	}
+	plbl(object){
+		animation_done = false;
+		var element = document.getElementById(object.element);
+		var i = 0;
+		var interval = setInterval(function(){
+			if (i<=object.message.length){
+				if(object.message.charAt(i) == "|"){
+					element.innerHTML += "<br>";
+				}
+				else if(object.message.charAt(i) == "^"){}
+				else{
+				element.innerHTML += object.message.charAt(i);
+				}
+				i++;
+			}
+			else{
+				animation_done = true;
+				clearInterval(interval);
+			}
+		}, object.speed);
+	}
+	command(input){
+		if(input == "y"){
+			if (this.context == ""){
+				g_e("start").style.display = "none";
+				g_e("startUp").style.display = "block";
+				this.add_animation(this.animation_fade, {element:"startUp"});
+				this.add_animation(function(){g_e("startUp").style.display = "none";});
+				this.add_animation(this.plbl, {message:"initiliazing boot sequence............|reticulating squids....^^^^^^|dereticulating squids....^^^^^^|deregulating squids....^^^^^^|reregulating squids....^^^^^^|squids prepared...^^^^^^",element:"console", speed:75});
+				this.add_animation(this.clear_console);
+				this.add_animation(this.plbl, {message:"SquidOS.^^^^^^.^^^^^^.^^^^^^ booted!^^^^^^^^^^^^^^", element:"console", speed:75})
+				this.add_animation(this.clear_console);
+				this.add_animation(this.plbl, {message:"Awating command. Type help for a list of commands.", speed:75, element:"console"});
+				this.context = "started";
+			}
+		}
+		if(input == "help"){
+			this.add_animation(this.clear_console);
+			this.add_animation(this.plbl, {element:"console", message:"List of avialable commands : ^^^^|...|^^^^^^give me a sec.^^^^.^^^^^.^^^^^^^^^ ok  I got them.|^^^^^^^^ Oh dear, it seems that we forgot to add the commands...| ^^^^^^^^ Oh well, just looks like we will have to reboot.", speed:75});
+			this.add_animation(function(){this.clear_console();g_e("startUp").style.display = "block"; this.animation_fade({element:"startUp"});})
+		}
+	}
+	get_input(){
+		var x = g_e("input").value;
+		g_e("input").value = "";
+		return x;
+	}
+	clear_console(){
+		g_e("console").innerHTML = "";
+	}
+	key_checker(event){
+		var key = event.keyCode;
+		if (key == 13){
+			this.command(this.get_input());
+			return false;
+		}
+	}
+	start(){
+		var that = this;
+		setInterval(function(){that.start_working();}, 1);
+		setInterval(function(){that.start_animating();}, 1);
+		this.animation_blink();
+	}
+}
+alert("compiled");
+main = new Main();
+main.start()
